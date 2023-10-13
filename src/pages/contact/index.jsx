0aa05eb622a57c27987d8c360fcf82177355d7cb
ankan112 <ankanhalder112@gmail.com>
2023-10-14 05:40:31 +0600

    contact page update

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,9 +1,105 @@
 import RootLayout from "@/components/layout/RootLayout";
 
+// import Button from "@/components/UI/Button";
+import { Input } from "antd";
+import React from "react";
+
 const ContactUs = () => {
+  const { TextArea } = Input;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Contact us page</h1>
+    <div className="main-container my-32 lg:my-36">
+      <div className="flex flex-col md:flex-row gap-6 lg:gap-14">
+        <div className="w-full md:w-[50%] md:px-4">
+          <h1 className="break-words font-montserrat font-semibold mb-4 text-4xl leading-7 text-gray-800">
+            Let&#39;s stay connected
+          </h1>
+          <p className="text-gray-600 text-md mt-4 lg:mt-6 mb-8 lg:mb-10">
+            We are always looking for new opportunities to work together. We are
+            always looking for new opportunities to work together.
+          </p>
+          <form className="flex flex-col gap-6">
+            <div className="flex justify-between gap-4">
+              <input
+                type="text"
+                id="firstName"
+                placeholder="First Name"
+                className="font-sans appearance-none border-2 border-gray-400 border-t-gray-100 border-l-gray-50 rounded w-full py-3 px-3 text-gray-700 leading-tight  focus:shadow-outline"
+              />
+              <input
+                type="text"
+                id="lastName"
+                placeholder="Last Name"
+                className="font-sans appearance-none border-2 border-gray-400 border-t-gray-100 border-l-gray-50 rounded w-full py-3 px-3 text-gray-700 leading-tight  focus:shadow-outline"
+              />
+            </div>
+            <input
+              type="text"
+              id="email"
+              placeholder="Email"
+              className="font-sans appearance-none border-2 border-gray-400 border-t-gray-100 border-l-gray-50 rounded w-full py-3 px-3 text-gray-700 leading-tight  focus:shadow-outline"
+            />
+            <div className="flex justify-between gap-4">
+              <input
+                type="text"
+                id="phone"
+                placeholder="Phone"
+                className="font-sans appearance-none border-2 border-gray-400 border-t-gray-100 border-l-gray-50 rounded w-full py-3 px-3 text-gray-700 leading-tight  focus:shadow-outline"
+              />
+              <input
+                type="text"
+                id="city"
+                placeholder="City"
+                className="font-sans appearance-none border-2 border-gray-400 border-t-gray-100 border-l-gray-50 rounded w-full py-3 px-3 text-gray-700 leading-tight  focus:shadow-outline"
+              />
+            </div>
+            <textarea
+              className="font-sans resize-y rounded-md w-full min-h-[150px] p-3 border-2 border-gray-400  text-gray-700 leading-tight  focus:shadow-outline"
+              placeholder="Type Your Message"
+            ></textarea>
+            {/* <Button btnName="Send Message" styles="w-full py-3"></Button> */}
+          </form>
+        </div>
+        <div className="w-full md:w-[50%] flex flex-col justify-center gap-6 text-gray-800">
+          <div className="py-6" style={{ borderBottom: "1px solid lightgray" }}>
+            <div
+              className="border-l-gray-500 px-4"
+              style={{ borderLeft: "4px solid" }}
+            >
+              <p>Email Us:</p>
+              <h4 className="text-xl font-bold underline mt-2">
+                Info@gmail.com
+              </h4>
+            </div>
+          </div>
+          <div className="py-6" style={{ borderBottom: "1px solid lightgray" }}>
+            <div
+              className="border-l-gray-500 px-4"
+              style={{ borderLeft: "4px solid" }}
+            >
+              <p>Call Us:</p>
+              <h4 className="text-xl font-bold underline mt-2">
+                (406) 555-0120
+              </h4>
+            </div>
+          </div>
+          <div className="py-6" style={{ borderBottom: "1px solid lightgray" }}>
+            <div
+              className="border-l-gray-500 px-4"
+              style={{ borderLeft: "4px solid" }}
+            >
+              <p>Office Address:</p>
+              <h4 className="text-xl font-bold mt-2">
+                1901 Thornridge Cir, Shiloh, Hawaii 81063
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
