a7529adf0fa8890c7d4c985b4a3473415845291f
ankan112 <ankanhalder112@gmail.com>
2023-10-31 01:52:56 +0600

    footer update

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,136 +1,83 @@
 import React from "react";
-import { SiInstagram } from "react-icons/si";
-import { TfiTwitter } from "react-icons/tfi";
-import { FaFacebookF } from "react-icons/fa";
+import { BsYoutube } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
 import { BiLogoLinkedin } from "react-icons/bi";
+import { IoLogoWhatsapp } from "react-icons/io";
 import Link from "next/link";
 import ScrollToTopButton from "../shared/ScrollToTopButton";
 
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <div className="bg-[#2F3137] mt-10">
+    <div className="bg-[#2F3137]">
       <footer className="main-container relative pt-8 pb-6">
         <div className="container mx-auto">
-          <div className="flex flex-wrap text-left lg:text-left">
-            <div className="w-full lg:w-6/12">
+          <div className="flex justify-center">
+            <div className="text-center">
               <h4 className="text-3xl font-semibold text-white">
                 Creative Crew
               </h4>
-              <h5 className="text-lg mt-0 mb-2 text-white">
+              <h5 className="text-lg mb-4 mt-2 text-white">
                 Capturing Moments, Creating Memories.
               </h5>
-              <div className="mt-6 lg:mb-0 mb-6 flex">
-                <button
-                  className="border-none bg-white text-black text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
-                >
-                  <SiInstagram className="w-full h-full p-2" />
-                </button>
-                <button
-                  className="border-none bg-white text-black text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
-                >
-                  <TfiTwitter className="w-full h-full p-2" />
-                </button>
-                <button
-                  className="border-none bg-white text-black shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
-                >
-                  <FaFacebookF className="w-full h-full p-2" />
-                </button>
-                <button
-                  className="border-none bg-white text-black text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
-                >
-                  <BiLogoLinkedin className="w-full h-full p-[6px]" />
-                </button>
-              </div>
-            </div>
-            <div className="w-full lg:w-6/12">
-              <div className="flex flex-wrap items-top mb-6">
-                <div className="w-full lg:w-4/12 px-4 ml-auto">
-                  <span className="block uppercase text-white text-sm font-bold mb-2">
-                    Useful Links
-                  </span>
-                  <ul className="list-unstyled list-none">
-                    <li>
-                      <Link
-                        className="text-white font-semibold block pb-2 text-sm"
-                        href="/about"
-                      >
-                        About Us
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="text-white font-semibold block pb-2 text-sm"
-                        href="#"
-                      >
-                        Blog
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        className="text-white font-semibold block pb-2 text-sm"
-                        href="#"
-                      >
-                        Service
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        className="text-white font-semibold block pb-2 text-sm"
-                        href="#"
-                      >
-                        Home
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="w-full lg:w-4/12 px-4">
-                  <span className="block uppercase text-white text-sm font-bold mb-2">
-                    Other Resources
-                  </span>
-                  <ul className="list-unstyled list-none">
-                    <li>
-                      <Link
-                        className="text-white font-semibold block pb-2 text-sm"
-                        href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile"
-                      >
-                        Our License
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="text-white font-semibold block pb-2 text-sm"
-                        href="https://creative-tim.com/terms?ref=njs-profile"
-                      >
-                        Terms &amp; Conditions
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="text-white font-semibold block pb-2 text-sm"
-                        href="https://creative-tim.com/privacy?ref=njs-profile"
-                      >
-                        Privacy Policy
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="text-white font-semibold block pb-2 text-sm"
-                        href="https://creative-tim.com/contact-us?ref=njs-profile"
-                      >
-                        Contact Us
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
             </div>
           </div>
-          <hr className="my-6 border-blueGray-300" />
+          <div className="flex flex-col md:flex-row justify-between w-full">
+            <div className="mt-2 lg:mb-0 mb-6 flex justify-center">
+              <button
+                className="border-none bg-white text-black text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-4"
+                type="button"
+              >
+                <BsYoutube className="w-full h-full p-2" />
+              </button>
+              <button
+                className="border-none bg-white text-black text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-4"
+                type="button"
+              >
+                <BsTwitter className="w-full h-full p-2" />
+              </button>
+              <button
+                className="border-none bg-white text-black shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-4"
+                type="button"
+              >
+                <BsFacebook className="w-full h-full p-2" />
+              </button>
+              <button
+                className="border-none bg-white text-black text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-4"
+                type="button"
+              >
+                <BiLogoLinkedin className="w-full h-full p-[6px]" />
+              </button>
+              <button
+                className="border-none bg-white text-black text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-4"
+                type="button"
+              >
+                <IoLogoWhatsapp className="w-full h-full p-[6px]" />
+              </button>
+            </div>
+            <div className="flex justify-center mt-4">
+              <Link
+                href="/"
+                className="hover:underline font-medium duration-200 mr-5 px-2 py-2 rounded text-white no-underline"
+              >
+                <p>Home</p>
+              </Link>
+              <Link
+                href="/about"
+                className="hover:underline font-medium duration-200 mr-5 px-2 py-2 rounded text-white no-underline"
+              >
+                <p>About Us</p>
+              </Link>
+              <Link
+                href="/contact"
+                className="hover:underline font-medium duration-200 mr-5 px-2 py-2 rounded text-white no-underline"
+              >
+                <p className="">Contact Us</p>
+              </Link>
+            </div>
+          </div>
+          <hr className="my-6 border border-solid border-gray-500" />
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-6/12 px-4 mx-auto text-center">
               <div className="text-sm text-white font-semibold py-1">
--- src/pages/order/index.jsx
+++ src/pages/order/index.jsx
@@ -2,7 +2,7 @@ import RootLayout from "@/components/layout/RootLayout";
 import { Button, DatePicker, message } from "antd";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
-
+import Swal from "sweetalert2";
 const Order = () => {
   const router = useRouter();
   const {
@@ -12,14 +12,20 @@ const Order = () => {
     reset,
   } = useForm();
 
-  const [messageApi, contextHolder] = message.useMessage();
+  // const [messageApi, contextHolder] = message.useMessage();
 
   const onSubmit = (data) => {
     console.log(data);
     // router.push("/");
-    messageApi.open({
-      type: "success",
-      content: "Booking Successful!",
+    // messageApi.open({
+    //   type: "success",
+    //   content: "Booking Successful!",
+    // });
+    Swal.fire({
+      title: "Order Booked!",
+      text: "Go Dashboard And Make Payment.",
+      icon: "success",
+      confirmButtonText: "Okay",
     });
     reset();
   };
@@ -28,94 +34,111 @@ const Order = () => {
   };
   return (
     <>
-      {contextHolder}
-      <div className="main-container ">
-        <h1 className="text-center mt-10">
-          Provide necessary information for booking
-        </h1>
-        <div className="w-full md:w-1/3 shadow-2xl p-10 border border-solid border-gray-200 mx-auto mt-5">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="w-full mt-4">
-              <input
-                className="block w-full px-4 py-4 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
-                type="text"
-                placeholder="Full Name"
-                aria-label="Full Name"
-                {...register("fullName", { required: true })}
-                aria-invalid={errors.fullName ? "true" : "false"}
-              />
-              {errors.fullName?.type === "required" && (
-                <p className="text-red-600 mt-2" role="alert">
-                  Full Name is required*
-                </p>
-              )}
-            </div>
-            <div className="w-full mt-4">
-              <input
-                className="block w-full px-4 py-4 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
-                type="number"
-                placeholder="Phone Number"
-                aria-label="Phone Number"
-                {...register("phone", { required: true })}
-                aria-invalid={errors.phone ? "true" : "false"}
-              />
-              {errors.phone?.type === "required" && (
-                <p className="text-red-600 mt-2" role="alert">
-                  Phone Number is required*
-                </p>
-              )}
-            </div>
-            <div className="w-full mt-4">
-              <DatePicker
-                className="block w-full px-4 py-4 mt-2 text-black placeholder-black bg-white border border-solid border-black rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
-                onChange={onChange}
-              />
-            </div>
-            <div className="w-full mt-4">
-              <input
-                className="block w-full px-4 py-4 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
-                type="email"
-                placeholder="Email Address"
-                aria-label="Email Address"
-                {...register("email", { required: true })}
-                aria-invalid={errors.email ? "true" : "false"}
-              />
-              {errors.email?.type === "required" && (
-                <p className="text-red-600 mt-2" role="alert">
-                  Email is required*
-                </p>
-              )}
-            </div>
+      {/* {contextHolder} */}
+      <div
+        className=" bg-no-repeat bg-cover bg-center text-white py-20"
+        style={{
+          background:
+            "linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/booking.jpg')",
+        }}
+      >
+        <div className="main-container flex gap-5 flex-col-reverse md:flex-row justify-between items-center">
+          <div className="w-full md:w-1/2 lg:w-1/3 shadow-2xl p-10 bg-white border border-solid border-gray-200 rounded-md mt-5">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="w-full mt-4">
+                <input
+                  className="block w-full px-4 py-4 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                  type="text"
+                  defaultValue="Ankan"
+                  placeholder="Full Name"
+                  aria-label="Full Name"
+                  {...register("fullName", { required: true })}
+                  aria-invalid={errors.fullName ? "true" : "false"}
+                />
+                {errors.fullName?.type === "required" && (
+                  <p className="text-red-600 mt-2" role="alert">
+                    Full Name is required*
+                  </p>
+                )}
+              </div>
+              <div className="w-full mt-4">
+                <input
+                  className="block w-full px-4 py-4 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                  type="number"
+                  placeholder="Phone Number"
+                  aria-label="Phone Number"
+                  {...register("phone", { required: true })}
+                  aria-invalid={errors.phone ? "true" : "false"}
+                />
+                {errors.phone?.type === "required" && (
+                  <p className="text-red-600 mt-2" role="alert">
+                    Phone Number is required*
+                  </p>
+                )}
+              </div>
+              <div className="w-full mt-4">
+                <DatePicker
+                  className="block w-full px-4 py-4 mt-2 text-black placeholder-black bg-white border border-solid border-black rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                  onChange={onChange}
+                />
+              </div>
+              <div className="w-full mt-4">
+                <input
+                  className="block w-full px-4 py-4 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                  type="email"
+                  placeholder="Email Address"
+                  aria-label="Email Address"
+                  {...register("email", { required: true })}
+                  aria-invalid={errors.email ? "true" : "false"}
+                />
+                {errors.email?.type === "required" && (
+                  <p className="text-red-600 mt-2" role="alert">
+                    Email is required*
+                  </p>
+                )}
+              </div>
 
-            <div className="w-full mt-4">
-              <input
-                className="block w-full px-4 py-4 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
-                type="text"
-                placeholder="Address"
-                aria-label="address"
-                {...register("address", {
-                  required: "Address is required*",
-                })}
-                aria-invalid={errors.address ? "true" : "false"}
-              />
-              {errors.address && (
-                <p className="text-red-600 mt-2" role="alert">
-                  {errors.address.message}
-                </p>
-              )}
-            </div>
+              <div className="w-full mt-4">
+                <input
+                  className="block w-full px-4 py-4 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                  type="text"
+                  placeholder="Address"
+                  aria-label="address"
+                  {...register("address", {
+                    required: "Address is required*",
+                  })}
+                  aria-invalid={errors.address ? "true" : "false"}
+                />
+                {errors.address && (
+                  <p className="text-red-600 mt-2" role="alert">
+                    {errors.address.message}
+                  </p>
+                )}
+              </div>
 
-            <div className="flex items-center justify-between mt-4">
-              <Button
-                size="large"
-                htmlType="submit"
-                className="w-full"
-                type="primary"
-              >
-                Confirm Book
-              </Button>
-            </div>
-          </form>
+              <div className="flex items-center justify-between mt-4">
+                <Button
+                  size="large"
+                  htmlType="submit"
+                  className="w-full"
+                  type="primary"
+                >
+                  Confirm Book
+                </Button>
+              </div>
+            </form>
+          </div>
+          <div className="w-full md:w-1/2">
+            <h2 className="text-4xl mb-5">
+              Make Your Booking <br /> With Joy!
+            </h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit est
+              consequuntur ad deserunt voluptatem cumque numquam suscipit, totam
+              fugiat distinctio tenetur, accusamus deleniti mollitia quasi error
+              ut, rerum quod quam!
+            </p>
+          </div>
         </div>
       </div>
     </>
