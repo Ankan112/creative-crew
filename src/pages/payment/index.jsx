c7dadec9842e98c54d2b6f8dabf3b32d3381ef9b
ankan112 <ankanhalder112@gmail.com>
2024-01-08 02:03:06 +0600

    unused code remove in payment page

--- src/pages/payment/index.jsx
+++ src/pages/payment/index.jsx
@@ -1,55 +1,6 @@
 import RootLayout from "@/components/layout/RootLayout";
 import PaymentPageForm from "@/components/shared/PaymentPageForm";
 import TitleBorder from "@/components/shared/TitleBorder";
-import { Space, Table, Tag } from "antd";
-const columns = [
-  {
-    title: "Package Name",
-    key: "package",
-    dataIndex: "package",
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? "geekblue" : "green";
-          if (tag === "loser") {
-            color = "volcano";
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: "Phone",
-    dataIndex: "phone",
-    key: "phone",
-  },
-  {
-    title: "Price",
-    dataIndex: "price",
-    key: "price",
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
-  },
-
-  {
-    title: "Status",
-    key: "status",
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
 
 const Payment = () => {
   return (
