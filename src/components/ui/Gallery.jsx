5a28c981324c6ad60f8d751a371bf7629410b80b
ankan112 <ankanhalder112@gmail.com>
2024-01-09 20:32:32 +0600

    gallery image size reduce

--- src/components/ui/Gallery.jsx
+++ src/components/ui/Gallery.jsx
@@ -1,42 +1,51 @@
 import { Col, Row } from "antd";
+import g1 from "../../../public/gallery/g1.jpg";
+import g2 from "../../../public/gallery/g2.jpg";
+import g3 from "../../../public/gallery/g3.jpg";
+import g4 from "../../../public/gallery/g4.jpg";
+import g5 from "../../../public/gallery/g5.jpg";
+import g6 from "../../../public/gallery/g6.jpg";
+import g7 from "../../../public/gallery/g7.jpg";
+import g8 from "../../../public/gallery/g8.jpg";
+import g9 from "../../../public/gallery/g9.jpg";
 import Image from "next/image";
 
 const Gallery = () => {
   const elements = [
     {
-      url: "/gallery/g1.jpg",
+      url: g1,
       col: 6,
     },
     {
-      url: "/gallery/g2.jpg",
+      url: g2,
       col: 10,
     },
     {
-      url: "/gallery/g3.jpg",
+      url: g3,
       col: 8,
     },
     {
-      url: "/gallery/g4.jpg",
+      url: g4,
       col: 8,
     },
     {
-      url: "/gallery/g5.jpg",
+      url: g5,
       col: 6,
     },
     {
-      url: "/gallery/g6.jpg",
+      url: g6,
       col: 10,
     },
     {
-      url: "/gallery/g8.jpg",
+      url: g7,
       col: 10,
     },
     {
-      url: "/gallery/g9.jpg",
+      url: g8,
       col: 8,
     },
     {
-      url: "/gallery/g7.jpg",
+      url: g9,
       col: 6,
     },
   ];
@@ -48,9 +57,10 @@ const Gallery = () => {
           Our Photo Gallery
         </h1>
       </div>
-      <Row gutter={[16, 16]} className="mt-10">
+      <Row gutter={[12, 6]} className="mt-10">
         {elements?.map((data, index) => (
           <Col
+            className=""
             key={index}
             xs={24}
             sm={24}
@@ -59,11 +69,11 @@ const Gallery = () => {
             span={data.col}
           >
             <Image
-              className="h-full w-full"
+              className=" w-full"
               src={data.url}
               alt="wedding-image"
-              height={400}
-              width={300}
+              height={380}
+              width={280}
               responsive="true"
             ></Image>
           </Col>
