44b34ab4f1b60b8aee9137ffc381981e920063f3
ankan112 <ankanhalder112@gmail.com>
2024-01-09 21:34:35 +0600

    auth page design change

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -39,16 +39,16 @@ const LoginPage = () => {
           <AuthCredential />
           <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
             <div className="px-6 py-4">
-              <h3 className="mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200">
+              <h3 className="mt-3 text-2xl font-semibold text-center text-black dark:text-black">
                 Welcome Back
               </h3>
-              <p className="mt-1 text-center text-gray-500 dark:text-gray-400">
+              <p className="mt-1 text-center text-black dark:text-black">
                 Please Login
               </p>
               <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="w-full mt-4">
                   <input
-                    className="block w-full px-4 py-4 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                    className="block w-full px-4 py-4 mt-2 text-black placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
                     type="email"
                     placeholder="Email Address"
                     aria-label="Email Address"
@@ -63,7 +63,7 @@ const LoginPage = () => {
                 </div>
                 <div className="w-full mt-4">
                   <input
-                    className="block w-full px-4 py-4 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                    className="block w-full px-4 py-4 mt-2 text-black placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
                     type="password"
                     placeholder="Password"
                     aria-label="Password"
@@ -81,25 +81,30 @@ const LoginPage = () => {
                 <div className="flex items-center justify-between mt-4">
                   <a
                     href="#"
-                    className="text-sm text-gray-600 dark:text-gray-200 hover:text-gray-500"
+                    className="text-sm text-black dark:text-black hover:text-black"
                   >
                     Forget Password?
                   </a>
 
-                  <Button size="large" htmlType="submit" type="primary">
+                  <Button
+                    size="large"
+                    className="bg-white text-black border border-solid border-[#835858] hover:bg-[#835858] hover:text-white"
+                    htmlType="submit"
+                    type="primary"
+                  >
                     Log In
                   </Button>
                 </div>
               </form>
             </div>
             <div className="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
-              <span className="text-sm text-gray-600 dark:text-gray-200">
+              <span className="text-sm text-black dark:text-black">
                 Don&#39;t have an account?{" "}
               </span>
 
               <Link
                 href="/register"
-                className="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline"
+                className="mx-2 text-sm font-bold text-primary hover:underline"
               >
                 Register
               </Link>
